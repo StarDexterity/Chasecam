ed98e9eae2b43d8f8de86ba0c6e08440a4ee051a
StarDexterity <72377348+StarDexterity@users.noreply.github.com>
2021-08-10 21:00:04 +1000

    Create rccar.ino

---
+++ rccar/rccar.ino
@@ -0,0 +1,12 @@
+// RC Car driver
+// Receives serial input from remote controller
+// Drives the motors
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
